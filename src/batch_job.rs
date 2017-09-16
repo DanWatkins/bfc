149254847fc37343f49b58e3dd321452c85043ce
Dan Watkins <dwatkins@valpineware.com>
2017-09-16 15:36:50 -0500

    Scans recursively and only processes files

--- src/batch_job.rs
+++ src/batch_job.rs
@@ -1,5 +1,7 @@
-use std::fs;
-use std::path;
+use std::fs::{self, DirEntry};
+use std::io;
+use std::path::Path;
+use std::path::PathBuf;
 use std::process::Command;
 
 pub struct BatchJob {
@@ -16,20 +18,58 @@ impl BatchJob {
     }
 
     pub fn run(&self) {
-        // run conversion
-        let paths = fs::read_dir(self.source_dir.trim()).unwrap();
+        let mut lst = vec!();
 
-        for path in paths {
-            self.convert_video(&path.unwrap().path(), self.destination_dir.trim());
+        // flatten
+        {
+            let mut add_it = |x: &DirEntry| {
+                lst.push(x.path());
+            };
+
+            match self.visit_dirs(Path::new(&self.source_dir.trim()), &mut add_it) {
+                Ok(x) => {},
+                Err(e) => {
+                    println!("{:?}", e);
+                    return;
+                }
+            };
+        }
+
+        println!("scanned {} files", lst.len());
+
+        for file in lst.iter() {
+            println!("converting {}", file.display());
+            self.convert_video(&file, self.destination_dir.trim());
         }
     }
 
-    fn convert_video(&self, source_filepath: &path::Path, output_dir: &str) {
-        let output_filepath = format!("{}{:?}.mp4", output_dir, source_filepath.file_stem().unwrap());
+    fn visit_dirs(&self, dir: &Path, cb: &mut FnMut(&DirEntry)) -> io::Result<()> {
+        if dir.is_dir() {
+            for entry in fs::read_dir(dir)? {
+                let entry = entry?;
+                let path = entry.path();
+                if path.is_dir() {
+                    self.visit_dirs(&path, cb)?;
+                } else {
+                    cb(&entry);
+                }
+            }
+        }
+        Ok(())
+    }
+
+    fn convert_video(&self, source_filepath: &Path, output_dir: &str) {
+        let mut out_file = PathBuf::new();
+        out_file.push(output_dir);
+        out_file.push(source_filepath.file_stem().unwrap());
+        out_file.set_extension("mp4");
 
         let mut output = Command::new("ffmpeg")
+            .arg("-loglevel")
+            .arg("warning")
             .arg("-i")
             .arg(source_filepath)
+            .arg("-y")
             .arg("-c:v")
             .arg("libx264")
             .arg("-preset")
@@ -40,9 +80,7 @@ impl BatchJob {
             .arg("1")
             .arg("-crf")
             .arg("20")
-            .arg("-c:a")
-            .arg("copy")
-            .arg(output_filepath)
+            .arg(out_file.as_path())
             .spawn()
             .unwrap_or_else(|e| {
                 panic!("failed to execute process: {}", e)
